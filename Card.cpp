44b79fc9a392d21343870fbdf285ab6da1bac32b
Alexander Sukach <alexsukach@gmail.com>
2023-11-08 18:14:26 -0500

    Card.cpp/h checked

--- Card.cpp
+++ Card.cpp
@@ -2,18 +2,14 @@
 #include <iostream>
 using namespace std;
 
-Card::Card() : rank(TWO), suit(HEARTS) {}
+Card::Card() : rank(TWO), suit(HEARTS) {} // 2H - default constructor value
 Card::Card(Rank r, Suit s) : rank(r), suit(s) {}
 
-Card::Rank Card::getRank() const {
-    return rank;
-}
-Card::Suit Card::getSuit() const {
-    return suit;
-}
-
+Card::Rank Card::getRank() const { return rank; }
+Card::Suit Card::getSuit() const { return suit; }
 
 void Card::display() const {
+    // Map card ranks and suits to strings for output
     const char* rankStr[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
     const char* suitStr[] = { "H", "C", "D", "S" };
     cout << rankStr[rank] << suitStr[suit] << endl;
--- Card.h
+++ Card.h
@@ -1,27 +1,18 @@
 #ifndef CARD_H
 #define CARD_H
 
-
 class Card {
-
-
     public:
-        enum Rank { TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE };
-        enum Suit { HEARTS, CLUBS, DIAMONDS, SPADES };
-
+        enum Rank { TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }; // All possible ranks
+        enum Suit { HEARTS, CLUBS, DIAMONDS, SPADES }; // All possible suits
         Card();
         Card(Rank r, Suit s);
-
         Rank getRank() const;
         Suit getSuit() const;
-
         void display() const;
-    private:
-        Rank rank;
-        Suit suit;
-
+        
+    private: 
+        Rank rank; // Type 'Rank' stores card rank within class
+        Suit suit; // Type 'Suit' stores card suit within class
 };
-
-
-
 #endif // CARD_H
